508dfbf653c99261040a5ae4c00a69c1ff3fb288
Manuel Arrieta Vallejo <marrieta.vallejo@gmail.com>
2021-10-16 20:53:32 -0500

    Using Replace Nested conditional with guard clauses

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,54 +14,53 @@ class GildedRose {
     }
 
     private void doUpdateQuality(Item item) {
-        if (!isAgedBrie(item)
-                && !isBackstagePasses(item)) {
-            if (item.quality > 0) {
-                if (!isSulfuras(item)) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
+        if (isAgedBrie(item)) {
+            item.sellIn = item.sellIn - 1;
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
-
-                if (isBackstagePasses(item)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
             }
-        }
 
-        if (!isSulfuras(item)) {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!isAgedBrie(item)) {
-                if (!isBackstagePasses(item)) {
-                    if (item.quality > 0) {
-                        if (!isSulfuras(item)) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
+            if (item.sellIn < 0) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
             }
+
+        } else if (isBackstagePasses(item)) {
+            item.sellIn = item.sellIn - 1;
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+
+            if (item.sellIn < 0) {
+                item.quality = item.quality - item.quality;
+            }
+        } else if (isSulfuras(item)) {
+
+        } else {
+            item.sellIn = item.sellIn - 1;
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+            }
         }
+
     }
 
     private boolean isSulfuras(Item item) {
